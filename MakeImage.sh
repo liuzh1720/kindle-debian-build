071fb2ca1ee0c38011645760f8c7a97ed2a1cbd5
Zhenghao Liu <popcornsniper1720@gmail.com>
2026-07-16 22:00:40 +0800

    Update debootstrap to use 'wheezy' instead of 'squeeze'

--- MakeImage.sh
+++ MakeImage.sh
@@ -4,6 +4,6 @@ mkfs.ext3 debian.ext3
 tune2fs -i 0 -c 0 debian.ext3
 mkdir /mnt/debian
 mount -o loop -t ext3 debian.ext3 /mnt/debian
-debootstrap --arch=armel --foreign squeeze /mnt/debian http://archive.debian.org/debian/
+debootstrap --arch=armel --foreign --no-check-gpg wheezy /mnt/debian http://archive.debian.org/debian/
 umount /mnt/debian
 rmdir /mnt/debian
