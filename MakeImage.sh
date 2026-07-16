a85f66443c59e99a28d9e4f8313abe626c4e9212
Zhenghao Liu <popcornsniper1720@gmail.com>
2026-07-16 21:46:04 +0800

    Add MakeImage.sh

---
+++ MakeImage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dd if=/dev/zero of=debian.ext3 bs=1M count=1024
+mkfs.ext3 debian.ext3
+tune2fs -i 0 -c 0 debian.ext3
+mkdir /mnt/debian
+mount -o loop -t ext3 debian.ext3 /mnt/debian
+debootstrap --arch=armel --foreign squeeze /mnt/debian http://archive.debian.org/debian/
+umount /mnt/debian
+rmdir /mnt/debian
